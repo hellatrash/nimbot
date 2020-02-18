d40abcc702c704aa89f6f97d4255888b25f75374
hella.trash_ <angelalli0405@yahoo.com>
2020-02-18 16:22:09 +0000

    added coinflip command to bot

--- nimbot.py
+++ nimbot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -12,7 +13,11 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
+@bot.command(name='coinflip')
+async def coinflip(ctx):
+    await ctx.send(random.choice(["heads", "tails"]))
+
 bot.run(token)
 
 #allison
-#omggg
+
