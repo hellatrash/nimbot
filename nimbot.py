d7fdbbd35256c644bd34a8f28a78befb11c2e434
hella.trash_ <angelalli0405@yahoo.com>
2020-02-20 19:10:18 +0000

    Added pickup lines under the name of "surprise" at random.

--- nimbot.py
+++ nimbot.py
@@ -17,9 +17,22 @@ async def play_nim(ctx):
 async def coinflip(ctx):
     await ctx.send(random.choice(["heads", "tails"]))
 
-@bot.command(name="average")
-async def average(ctx, a, b):
-    await ctx.send( (a+b)/2)
+@bot.command(name="surprise")
+async def surprise(ctx):
+    await ctx.sent(random.choice(["Do you live in a corn field, cause I'm stalking you.",
+                          "I’m not a hoarder but I really want to keep you forever.",  
+                          "You're so beautiful that you made me forget my pickup line.",
+                          "Are you lost ma'am? Because heaven is a long way from here.", 
+                          "Four plus four equals eight, but you plus me equals fate.", 
+                          "I must be a snowflake, because I've fallen for you.", 
+                          "You must be a hell of a thief because you stole my heart from across the room.",
+                          "If you were a booger I'd pick you first.", 
+                          "If you were a chicken, you'd be impeccable.",
+                          "Life without you would be like a broken pencil... pointless,"]))
+
+
+
+
 
 bot.run(token)
 
